33785e07afc6f4e3f2ab9ef05d9ee9f0e54c8cda
cfwp007 <554074339@qq.com>
2021-08-02 17:20:51 +0800

    百分比布局

--- app/src/main/java/com/xunixianshi/vrshow/testdemo/MainActivity.kt
+++ app/src/main/java/com/xunixianshi/vrshow/testdemo/MainActivity.kt
@@ -13,7 +13,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        setContentView(R.layout.test_main_layout4)
 
     }
 
