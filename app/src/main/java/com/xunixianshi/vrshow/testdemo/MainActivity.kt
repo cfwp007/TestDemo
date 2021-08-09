5c40c98c65e6a79f6bd27e4273671c82290feca8
cfwp007 <554074339@qq.com>
2021-08-09 15:33:00 +0800

    changed main layout

--- app/src/main/java/com/xunixianshi/vrshow/testdemo/MainActivity.kt
+++ app/src/main/java/com/xunixianshi/vrshow/testdemo/MainActivity.kt
@@ -13,8 +13,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-        setContentView(R.layout.test_main_layout4)
+        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.test_main_layout4)
 
     }
 
